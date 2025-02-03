7dc17fa0c93216072eb72b25592e042325f3b82c
Donjella <brendonang@gmail.com>
2025-02-03 20:28:24 +1100

    feat: 404 route and error handler middleware

--- src/server.js
+++ src/server.js
@@ -8,10 +8,32 @@ const app = express();
 
 app.get("/", (request, response) => {
 	response.json({
-        message: "Hello, World!"
-    })
+		message: "Hello, world!"
+	});
+});
+
+// Wildcard * means "match any route"
+// Put this at the end of your route declarations
+// to catch anything that does not match an earlier route
+app.get("*", (request, response) => {
+	console.log("User tried to visit " + request.path);
+	response.json({
+		message:"Page not found.",
+		attemptedPath: request.path
+	});
+});
+
+// Error handling catcher 
+// applies to EVERY route in the entire server
+app.use((error, request, response, next) => {
+	console.log("Error occured in the server.");
+	console.log(JSON.stringify(error));
+	response.json({
+		errors: request.body.errors,
+		message: error.message
+	});
 });
 
 module.exports = {
-    app
+	app
 }
