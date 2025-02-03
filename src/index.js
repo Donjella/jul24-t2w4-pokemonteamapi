906f0b6a8e0b76fae44e0914c05eed740a63f7ef
Donjella <brendonang@gmail.com>
2025-02-03 20:20:08 +1100

    feat: port controlled by env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // Import the configured server 
 // and run it 
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const { app } = require("./server");
 
-app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log("Server is running on port " + PORT);
 });
